314ead6b388b4b8a3296d56ed374d43ca035b927
Ryan Young <youngre511@gmail.com>
2024-04-16 18:52:08 -0400

    style: fix layout of trainer cards and move/trainer lists

--- public/js/allTrainers.js
+++ public/js/allTrainers.js
@@ -6,4 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = `/trainer/${div.id}`;
     });
   });
+
+  const allTrainers = document.querySelector(".all-trainers");
+  let trainersWidth = allTrainers.clientWidth - 60;
+  let columnNum = ~~(trainersWidth / 320);
+  let columnGap = (trainersWidth - columnNum * 300) / (columnNum - 1);
+  allTrainers.style.gridTemplateColumns = `repeat(${columnNum}, 300px)`;
+  allTrainers.style.columnGap = `${columnGap}px`;
+
+  window.addEventListener("resize", () => {
+    let trainersWidth = allTrainers.clientWidth - 60;
+    console.log(trainersWidth);
+    let columnNum = ~~(trainersWidth / 320);
+    console.log(columnNum);
+    let columnGap = (trainersWidth - columnNum * 300) / (columnNum - 1);
+    console.log(columnGap);
+    allTrainers.style.gridTemplateColumns = `repeat(${columnNum}, 300px)`;
+    allTrainers.style.columnGap = `${columnGap}px`;
+  });
 });
